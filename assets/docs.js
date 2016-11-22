55541d612af2f95617c36f6a11851c3641cb0b41
doup <asier@illarra.com>
2016-11-22 18:54:04 +0100

    Update 2016-11-22T17:53:58.981Z

---
+++ assets/docs.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('[data-components-menu-toggle]').hover(function () {
+        $('[data-components-menu]').show();
+    })
+    
+    $('[data-components-menu]').mouseleave(function () {
+        $(this).hide()
+    });
+});
